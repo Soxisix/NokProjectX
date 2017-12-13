77defd42de3e02e41d8dfe9720b772120dcac208
ivandeepocean <ivanlouiscamus@gmail.com>
2017-12-13 23:55:42 +0800

    a

---
+++ NokProjectX.Wpf/NokProjectX.Wpf/Entities/Invoice.cs
@@ -0,0 +1,12 @@
+﻿namespace NokProjectX.Wpf.Entities
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+
+        public virtual Product Product { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double Size { get; set; }
+    }
+}
--- NokProjectX.Wpf/NokProjectX.Wpf/ViewModel/Transaction/TransactionViewModel.cs
+++ NokProjectX.Wpf/NokProjectX.Wpf/ViewModel/Transaction/TransactionViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
 using System.Windows.Documents;
+using GalaSoft.MvvmLight.Command;
 using NokProjectX.Wpf.Common.Messages;
 using NokProjectX.Wpf.Common.Validator;
 using NokProjectX.Wpf.Context;
@@ -17,6 +20,55 @@ namespace NokProjectX.Wpf.ViewModel.Transaction
             _context = context;
             LoadData();
             MessengerInstance.Register<RefreshMessage>(this, OnRefresh);
+            LoadCommand();
+            InvoiceList = new ObservableCollection<Invoice>();
+        }
+
+        private void LoadCommand()
+        {
+            AddInvoiceCommand = new RelayCommand(OnAddInvoice);
+            RemoveInvoiceCommand = new RelayCommand(OnRemoveInvoice);
+        }
+
+        public RelayCommand RemoveInvoiceCommand { get; set; }
+
+        private void OnRemoveInvoice()
+        {
+            if (SelectedInvoice != null)
+            {
+                InvoiceList.Remove(SelectedInvoice);
+            }
+        }
+
+        public RelayCommand AddInvoiceCommand { get; set; }
+
+        private void OnAddInvoice()
+        {
+            var newInvoice = new Invoice()
+            {
+                Product = SelectedProduct,
+                Price = Price,
+                Quantity = Quantity.GetValueOrDefault()
+                
+            };
+            InvoiceList.Add(newInvoice);
+        }
+
+        
+        public int? Quantity { get { return _quantity; } set { Set(ref _quantity, value); } }
+
+
+        public double Price
+        {
+            get
+            {
+                return _price;
+                
+            }
+            set
+            {
+                Set(ref _price, value);
+            }
         }
 
         private void OnRefresh(RefreshMessage obj)
@@ -36,7 +88,35 @@ namespace NokProjectX.Wpf.ViewModel.Transaction
 
 
         private Product _selectedProduct;
-        public Product SelectedProduct { get { return _selectedProduct; } set { Set(ref _selectedProduct, value); } }    
+        private ObservableCollection<Invoice> _invoiceList;
+        private double _price;
+        private int? _quantity;
+        private Invoice _selectedInvoice;
+
+        public Product SelectedProduct
+        {
+            get
+            {
+                return _selectedProduct;
+            }
+            set
+            {
+                Set(ref _selectedProduct, value);
+                if (value != null)
+                {
+                    Price = value.Price;
+                }
+                
+            }
+        }
+
+        public ObservableCollection<Invoice> InvoiceList
+        {
+            get { return _invoiceList; }
+            set { Set(ref _invoiceList, value); }
+        }
+
+        public Invoice SelectedInvoice { get { return _selectedInvoice; } set { Set(ref _selectedInvoice, value); } }    
 
     }
 }
