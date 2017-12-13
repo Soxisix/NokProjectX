6e933177dc0cd3dffae9a659a3f72b978ed5628e
ivandeepocean <ivanlouiscamus@gmail.com>
2017-12-13 20:45:26 +0800

    a

---
+++ NokProjectX.Wpf/NokProjectX.Wpf/ViewModel/Login/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NokProjectX.Wpf.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NokProjectX.Wpf.ViewModel.Login
+{
+    public class LoginViewModel
+    {
+
+    }
+}
